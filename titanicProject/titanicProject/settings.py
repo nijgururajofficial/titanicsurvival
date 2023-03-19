a6b0fc084ede705e2ee1ab4fcfba046595152b68
nijgururajofficial <nijgururaja12@gmail.com>
2023-03-19 15:32:22 +0530

    first commit

--- titanicProject/titanicProject/settings.py
+++ titanicProject/titanicProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8tg$g_o1jhymo$b4d45loqg!yipf&xrdwtsbn6szo!43o37z@6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
